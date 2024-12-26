7ede71765f3c17efd7091bba445cb55efa76cd56
megops <megan.fcoyle@gmail.com>
2024-12-25 21:47:17 -0500

    Added the get_book_char function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    book_char = get_book_char(text)
     print(f"{num_words} words found in the document")
+    print(f"Unique letters:{book_char}")
 
 
 def get_num_words(text):
@@ -13,5 +15,15 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_book_char(text):
+    uniqueCharacters = {}
+    for i in text:
+        lower=i.lower()
+        if lower in uniqueCharacters:
+            uniqueCharacters[lower] +=1
+        else:
+            uniqueCharacters[lower] = 1
+    return (uniqueCharacters)
 
 main()
